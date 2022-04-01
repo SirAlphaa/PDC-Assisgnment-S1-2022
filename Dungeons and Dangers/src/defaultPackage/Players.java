55807199a60d46139d44b2a01ee9e22a5ef99894
NathanDomFlanagan <jaypedesh7@gmail.com>
2022-04-02 12:52:26 +1300

    bebop

--- Dungeons and Dangers/src/defaultPackage/Players.java
+++ Dungeons and Dangers/src/defaultPackage/Players.java
@@ -2,12 +2,12 @@ public class Players
 {
    //TODO: Use buffer reader and printwriter to check if the user exits, or make a new one and save progress with hash map.
    
-   public static void newPlayer()
+   public static void createPlayer()
    {
     
    }
 
-   public static void resumePlayer()
+   public static void existingPlayer()
    {
 
    }
