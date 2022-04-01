70e38c07f871d06d9303e9ccfeb12dead2591378
NathanDomFlanagan <jaypedesh7@gmail.com>
2022-04-02 12:50:58 +1300

    Added player class and player text file too add new players or check for new noes B)

---
+++ Dungeons and Dangers/src/defaultPackage/Players.java
@@ -0,0 +1,14 @@
+public class Players 
+{
+   //TODO: Use buffer reader and printwriter to check if the user exits, or make a new one and save progress with hash map.
+   
+   public static void newPlayer()
+   {
+    
+   }
+
+   public static void resumePlayer()
+   {
+
+   }
+}
