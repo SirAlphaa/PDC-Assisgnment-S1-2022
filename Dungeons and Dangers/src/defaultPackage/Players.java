ac388db5083721be19515021087e42e29d77cbb2
NathanDomFlanagan <jaypedesh7@gmail.com>
2022-04-02 13:08:03 +1300

    added hashmap to player class

--- Dungeons and Dangers/src/defaultPackage/Players.java
+++ Dungeons and Dangers/src/defaultPackage/Players.java
@@ -1,7 +1,11 @@
+import java.util.HashMap;
+
 public class Players 
 {
    //TODO: Use buffer reader and printwriter to check if the user exits, or make a new one and save progress with hash map.
    
+   HashMap<String, Integer> Player = new HashMap<String, Integer>();
+
    public static void createPlayer()
    {
     
