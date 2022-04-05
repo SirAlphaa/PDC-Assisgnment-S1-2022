fd80508ebcb3ac3a8cbcfc6ae40cb20a65253f66
NathanDomFlanagan <jaypedesh7@gmail.com>
2022-04-05 14:56:49 +1200

    Worked on UserCharacter class B)

--- Dungeons and Dangers/src/Cave.java
+++ Dungeons and Dangers/src/Cave.java
@@ -1,4 +1,16 @@
 public class Cave 
 {
-  
+  public static void Scenario()
+  {
+    boolean type = false;
+    int intInput = 0;
+
+    while (type == true)
+    {
+      if (intInput == 1)
+      {
+        
+      }
+    }
+  }
 }
--- Dungeons and Dangers/src/UserCharacter.java
+++ Dungeons and Dangers/src/UserCharacter.java
@@ -8,9 +8,40 @@ public class UserCharacter
 
    public static void createPlayer()
    {
-    
+      String name;
+      Integer attributes = 000;
+      Integer input = 000;
+     
+      FancyPrint.setDelay("Whats your name, adventurer?: ", 10, true);
+      //TODO: Add scanner to input name
+      FancyPrint.setDelay("Input your character loadout in order of sex, class and weapon: ", 10, true);
+      FancyPrint.setDelay("100 is Male and 200 is Female", 10, true);
+      FancyPrint.setDelay("010 is a Mage and 020 is a Knight", 10, true);
+      FancyPrint.setDelay("001 is a Staff and 002 is a Sword", 10, true);
+
+      if (input.equals("111"))
+      {
+         FancyPrint.setDelay("You are a Male Mage, wielding a staff!", 10, true);
+         attributes = 111;
+      }
+      else if (input.equals("211"))
+      {
+         FancyPrint.setDelay("You are a Female Mage, wielding a staff!", 10, true);
+         attributes = 211;
+      }
+      else if (input.equals("122"))
+      {
+         FancyPrint.setDelay("You are a Male Knight, wielding a sword!", 10, true);
+         attributes = 122;
+      }
+      else if (input.equals("222"))
+      {
+         FancyPrint.setDelay("You are a Female Knight, wielding a Sword!", 10, true);
+         attributes = 222;
+      }
    }
 
+   //Use FileReader to read the users Name and Attributes from a text file
    public static void existingPlayer()
    {
 
