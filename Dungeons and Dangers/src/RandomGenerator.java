fdc92cc9ddc9dcec63c00a9cb5b35140dc930940
NathanDomFlanagan <jaypedesh7@gmail.com>
2022-04-12 14:52:14 +1200

    amongus

--- Dungeons and Dangers/src/RandomGenerator.java
+++ Dungeons and Dangers/src/RandomGenerator.java
@@ -1,4 +1,25 @@
+import java.util.Random;
+
 public class RandomGenerator 
 {
-  
+  public static void userDamageChance()
+  {
+    Random userDamageChance = new Random();
+    int damageChance = userDamageChance.nextInt();
+  }
+
+  public static void userDefendChance()
+  {
+
+  } 
+
+  public static void enemyDamageChance()
+  {
+
+  }
+
+  public static void enemyDefendChance()
+  {
+
+  }
 }
