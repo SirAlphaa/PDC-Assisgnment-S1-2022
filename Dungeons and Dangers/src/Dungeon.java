2fbc7c565a16a10721e116c091dec819a57a4667
NathanDomFlanagan <jaypedesh7@gmail.com>
2022-04-15 16:49:56 +1200

    a

--- Dungeons and Dangers/src/Dungeon.java
+++ Dungeons and Dangers/src/Dungeon.java
@@ -1,14 +1,35 @@
 public class Dungeon 
 {
+  public static void amountOfRound()
+  {
+    FancyPrint.setDelay("How many rooms would you like to clear? ", 10, true);
+    int userInpute = 0;
+    //TODO: Work on this.
+  }
+  
   public static void introToDungeon()
   {
     //TODO: Replace all sysout with the fancy print one B).
-    System.out.println("Write some introduciton to the dungeon");
+    FancyPrint.setDelay("QUEST LOCATION: The Dungeons of Dangers", 10, true);
+    FancyPrint.setDelay("DUNGEON LEVEL: EXTREMELY DANGEROUS", 10, true);
+    FancyPrint.setDelay("QUEST GOAL: Clear out the dungeon to protect the citizens of Windhelm from the enemies", 10, true);
+    Main.sleep(5000);
+    Main.clearScreen();
+    FancyPrint.setDelay("You ascend the mountain to enter the accursed cave. . .", 10, true); //TODO: maybe add fancy loading dots.
+    FancyPrint.setDelay("You descend into the depths of the dungeon. . .", 10, true);
   }
 
+  //Used after defeating the first enemy
   public static void choiceOfDirection()
   {
-    System.out.println("Write something about encountering two pathways, one has weird sounds and the other is silent type of thing");
+    System.out.println("You approach a split in the narrow hallways, the left is a dugged out hole and the right looks like it was destroyed by something unsual");
+    Main.sleep(2000);
+    Main.clearScreen();
+    FancyPrint.setDelay("Which direction do you choose? ", 10, true);
+    FancyPrint.setDelay(" (1) for left", 10, true);
+    FancyPrint.setDelay(" (2) for right", 10, true);
+
+    
   }
 
   public static void leftPath()
@@ -19,5 +40,6 @@ public class Dungeon
   public static void rightPath()
   {
     System.out.println("Path goes on for a while or something idk, then you encounter another 2 pathways, except one has been dug out really bad, like a beast has dug through it");
+
   }
 }
--- Dungeons and Dangers/src/createCharacter.java
+++ Dungeons and Dangers/src/createCharacter.java
@@ -5,89 +5,51 @@ public class CreateCharacter
 {
   public static UserCharacter createPlayer(UserCharacter userCharacter)
   {
+    //The Hashmap.
     HashMap<String, Integer> Player = new HashMap<>();
     Player = FileInterface.readRecordHashMap(Player, "userCharacter.txt");
-    //name (key) for hashmap
-    String name = ""; 
-    //attribute (value) for hashmap
-    Integer attributes = 000;
-
-    //Printing out basic text to introduce a new character creation
+    
+    //====================================================================================================================
+    //Printing out basic text to introduce a new character creation.
     FancyPrint.setDelay("Whats your name, Adventurer?: ", 20 , true);
-    //TODO: Use buffer reader and printwriter to check if the user exits, or make a new one and save progress with hash map.
-    String inputName = CustomScan.scanString();
-    name = inputName;
-    
-    //FIXME: Fix the english for this.
-    FancyPrint.setDelay("Input your character loadout in order of sex, class and weapon: ", 10, true);
-    FancyPrint.setDelay("<100> is Male and <200> is Female", 10, true);
-    FancyPrint.setDelay("<010> is a Mage and <020> is a Knight", 10, true);
-    FancyPrint.setDelay("<001> is a Staff and <002> is a Sword", 10, true);
-    FancyPrint.setDelay("You cannot have <010> (Mage) and <002> (Sword),", 10, true);
-    FancyPrint.setDelay("or <020> (Knight) and <001> (Staff) as a loadout!", 10, true);
+    //name (key) for hashmap
+    String name = CustomScan.scanString();
 
-    //Scanner used to input attributes as an integer value
-    //TODO: Fix yo if statement for this
-    Integer input = CustomScan.scanInt();
+    //====================================================================================================================
+    //Use prints and scanner to ask what your loadout will be.
+    FancyPrint.setDelay("Input your character loadout in order of sex, class and weapon: \n", 10, true);
+    FancyPrint.setDelay("To be a Male input (1) and to become a Female input (2)", 10, true);
+   
+    //Saving input (attribute) of the sex.
+    Integer sexInput = CustomScan.scanInt();
 
+    FancyPrint.setDelay("To choose the class Mage input (1) and to choose the class Knight input (2), to be a Assassin input (3) and to become a Soldier input (4)", 10, true);
+   
+    //Saving input (attribute) of the class.
+    Integer classInput = CustomScan.scanInt();
 
-    //If statement depending on values chosen in scanner, e.g a male knight with a sword would be saved into the hashmap
-    // boolean newName = true;
-    // while (newName)
-    // {
-    //   if (name.equals(null))
-    //   {
-    //     newName = true;
-    //     FancyPrint.setDelay("Input a name please.", 10, true);
-    //   }
-    // }
-    //TODO: Perhaps add the ability for a mage to have a sword and knight to have a staff but they have drawbacks obviously.
-    boolean newAttribute = true;
-    while (newAttribute)
+    FancyPrint.setDelay("To choose a staff as your weapon input (1) and to choose a Sword as your weapon input (2) to use a Dagger input (3) and to use a Gun input (4)", 10, true);
+   
+    //Saving input (attribute) of the weapon.
+    Integer weaponInput = CustomScan.scanInt();
+
+    //====================================================================================================================
+    //Saving all three inputs of sex, class and weapon into one Ineteger to be input into the Hashmap.
+    Integer input = (sexInput * 100) + (classInput * 10) + weaponInput;
+
+    if (input.equals("DRAGON"))
     {
-      if (input.equals(111))
-      {
-        FancyPrint.setDelay("You are a Male Mage, wielding a staff!", 10, true);
-        attributes = 111;
-        Player.put(name, attributes);
-        newAttribute = false;
-      }
-      else if (input.equals(211))
-      {
-        FancyPrint.setDelay("You are a Female Mage, wielding a staff!", 10, true);
-        attributes = 211;
-        Player.put(name, attributes);
-        newAttribute = false;
-      }
-      else if (input.equals(122))
-      {
-        FancyPrint.setDelay("You are a Male Knight, wielding a sword!", 10, true);
-        attributes = 122;
-        Player.put(name, attributes);
-        newAttribute = false;
-      }
-      else if (input.equals(222))
-      {
-        FancyPrint.setDelay("You are a Female Knight, wielding a Sword!", 10, true);
-        attributes = 222;
-        Player.put(name, attributes);
-        newAttribute = false;
-      }
-      else if (input.equals(112) || input.equals(212) || input.equals(null))
-      {
-        newAttribute = true;
-        FancyPrint.setDelay("Wrong class and item combination! Try again please.", 10, true);
-      }
+      
     }
-    //TODO: Add the 'Player' hashmap into a text file.
-    //TODO: Fix yo character.txt file
+
+    //====================================================================================================================
+    userCharacter.setCharacter(name, input);
+    FancyPrint.setDelay("You are a " + userCharacter.getGender() + " " + userCharacter.getRole() + " wielding a " + userCharacter.getWeapon(), 10, true);
     
-    //=== DONT TOUCH THE READER I SWEAR TO GOD ====================================================
+    //====================================================================================================================
     FileInterface.writeRecordHasMap(Player, "userCharacter.txt");
     FancyPrint.setDelay("Selected and Saved :)", 20, true);
     Main.sleep(2000);
-
-    userCharacter.setCharacter(name, attributes);
     return userCharacter;
   }
 }
